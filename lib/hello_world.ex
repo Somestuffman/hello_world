b6725112d21245b5b02416344902cabbf169a4d5
Somestuffman <semen1232007@yandex.ru>
2021-11-22 23:46:15 +0300

    Initial commit

---
+++ lib/hello_world.ex
@@ -0,0 +1,18 @@
+defmodule HelloWorld do
+  @moduledoc """
+  Documentation for `HelloWorld`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> HelloWorld.greetings()
+      "Hello, world!"
+
+  """
+  def greetings do
+    "Hello, world!"
+  end
+end
